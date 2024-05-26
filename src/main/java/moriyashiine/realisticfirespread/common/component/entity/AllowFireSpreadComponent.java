6b3f699138c8988efaded2f2d9d1fc41307a86d6
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-05-26 16:52:54 -0500

    update license headers

--- src/main/java/moriyashiine/realisticfirespread/common/component/entity/AllowFireSpreadComponent.java
+++ src/main/java/moriyashiine/realisticfirespread/common/component/entity/AllowFireSpreadComponent.java
@@ -1,7 +1,6 @@
 /*
- * All Rights Reserved (c) MoriyaShiine
+ * Copyright (c) MoriyaShiine. All Rights Reserved.
  */
-
 package moriyashiine.realisticfirespread.common.component.entity;
 
 import net.minecraft.entity.Entity;
--- src/main/java/moriyashiine/realisticfirespread/mixin/PlayerEntityMixin.java
+++ src/main/java/moriyashiine/realisticfirespread/mixin/PlayerEntityMixin.java
@@ -1,7 +1,6 @@
 /*
- * All Rights Reserved (c) MoriyaShiine
+ * Copyright (c) MoriyaShiine. All Rights Reserved.
  */
-
 package moriyashiine.realisticfirespread.mixin;
 
 import moriyashiine.realisticfirespread.common.init.ModEntityComponents;
